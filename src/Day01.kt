e6b6d32f4b0c26084188448141dbdf4d97a70fec
Tom Seifert <dev@tpseifert.de>
2023-12-01 09:42:43 +0100

    Solve day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,50 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    val chaoticCalibrationInput = readInput("Day01")
+    // Part 1
+    chaoticCalibrationInput.mapNotNull { chaoticLine ->
+        val digits = chaoticLine.mapNotNull { it.digitToIntOrNull() }
+        if (digits.isEmpty()) return@mapNotNull null
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+        val firstDigit = digits.first()
+        val lastDigit = digits.last()
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+        "$firstDigit$lastDigit".toInt()
+    }.sum().println()
 
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    // Part 2
+    chaoticCalibrationInput.sumOf { chaoticLine ->
+        var remainingLine = chaoticLine
+        val foundDigits = mutableListOf<Int>()
+        while (remainingLine.isNotEmpty()) {
+            val digit = remainingLine.first().digitToIntOrNull()
+            if (digit != null) {
+                foundDigits.add(digit)
+            } else {
+                remainingLine.findTextNumber()?.let { foundDigits.add(it) }
+            }
+
+            remainingLine = remainingLine.drop(1)
+        }
+
+        if (foundDigits.isEmpty()) return@sumOf 0
+
+        val firstDigit = foundDigits.first()
+        val lastDigit = foundDigits.last()
+
+        "$firstDigit$lastDigit".toInt()
+    }.println()
+}
+
+fun String.findTextNumber(): Int? {
+    if (startsWith("one")) return  1
+    if (startsWith("two")) return 2
+    if (startsWith("three")) return 3
+    if (startsWith("four")) return 4
+    if (startsWith("five")) return 5
+    if (startsWith("six")) return 6
+    if (startsWith("seven")) return 7
+    if (startsWith("eight")) return 8
+    if (startsWith("nine")) return 9
+
+    return null
 }
